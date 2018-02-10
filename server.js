3d8df0e68b3de1e4157b61a373a18fe2b155b532
linxinzhe <linxinzhechn@icloud.com>
2018-02-10 22:41:08 +0800

    feat(eslint): static code anylysis

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-'use strict';
+'use strict'
 
-const http = require('http');
-const path = require('path');
-const express = require('express');
+const http = require('http')
+const path = require('path')
+const express = require('express')
 
-const port = process.env.PORT || 8888;
-const app = express();
-const server = http.createServer(app);
+const port = process.env.PORT || 8888
+const app = express()
+const server = http.createServer(app)
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist')))
 
-server.listen(port, () => console.log(`Server listening on port ${port}`));
+server.listen(port, () => console.log(`Server listening on port ${port}`))
---
+++ src/app.js
@@ -0,0 +1,7 @@
+const Vue = require('vue')
+const Vuex = require('vuex')
+const VueMaterial = require('vue-material')
+// const Web3 = require('web3')
+
+Vue.use(Vuex)
+Vue.use(VueMaterial)
