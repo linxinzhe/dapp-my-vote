cf058c3a8859999cabcc877ae32792dcc06fabb6
linxinzhe <linxinzhechn@icloud.com>
2018-02-11 18:01:08 +0800

    feat(app): main frame layout

--- src/app.js
+++ src/app.js
@@ -1,7 +1,23 @@
-const Vue = require('vue')
-const Vuex = require('vuex')
-const VueMaterial = require('vue-material')
+import Vue from 'vue'
+import Vuex from 'vuex'
+import VueMaterial from 'vue-material'
+import App from './components/app.vue'
+
 // const Web3 = require('web3')
 
 Vue.use(Vuex)
 Vue.use(VueMaterial)
+
+async function startApp () {
+  // eslint-disable-next-line no-unused-vars
+  const vm = new Vue({
+    el: '#app',
+    render (createElement) {
+      return createElement(App)
+    }
+  })
+}
+
+window.addEventListener('load', event => {
+  startApp()
+})
---
+++ src/components/app.vue
@@ -0,0 +1,57 @@
+<style>
+    .main-content {
+        padding: 16px;
+    }
+</style>
+
+<template>
+  <div>
+    <md-toolbar class="md-medium">
+      <h1 class="md-title" style="flex: 1">Simple Vote</h1>
+      <md-layout md-flex="40">
+        <md-layout md-flex="25">
+          <!--<web3></web3>-->
+        </md-layout>
+        <md-layout md-flex-offset="5">
+          <!--<account></account>-->
+        </md-layout>
+      </md-layout>
+      <md-button @click="openDialog" class="md-raised md-accent">Submit Proposal</md-button>
+    </md-toolbar>
+    <div class="main-content">
+      <md-layout md-column md-flex>
+        <md-layout>
+          <md-layout v-for="(p, hash) in proposals" :key="hash" md-flex="33" md-row>
+            <!--<proposal :hash="hash" style="flex: 1; margin: 5px"></proposal>-->
+          </md-layout>
+        </md-layout>
+        <md-layout>
+          <!--<transactions style="flex: 1"></transactions>-->
+        </md-layout>
+      </md-layout>
+    </div>
+    <md-snackbar
+      ref="notice"
+      md-position="top center"
+      md-duration="5000"
+      @close="onNoticeClose">
+        <span>{{notice}}</span>
+        <md-button @click="$refs.notice.close()">Close</md-button>
+      </md-snackbar>
+    <md-dialog-prompt
+      ref="propose"
+      v-model="proposal"
+      md-title="Submit Proposal"
+      md-ok-text="Submit"
+      md-cancel-text="Cancel"
+      md-input-maxlength="140"
+      md-content="Please write a proposal (max 140 chars)"
+      @close="onDialogClose"></md-dialog-prompt>
+  </div>
+</template>
+
+<script>
+module.exports = {
+  name: 'app',
+}
+</script>
